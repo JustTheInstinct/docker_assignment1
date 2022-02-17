e28901e0bd4eeb9053248b35c43ea1e12f3103e5
JustTheInstinct <jordangmain@gmail.com>
2022-02-16 17:37:20 -0800

    placeholder authentication python file

---
+++ auth.py
@@ -0,0 +1,11 @@
+import json
+
+def credentials(user, password):
+    if user == 'admin' and password == 'potato':
+        return 1
+    else:
+        return 0
+
+
+if __name__ == "__main__":
+    pass
