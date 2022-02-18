377c0118864dacf0dc786aa51a6ff1cbc6217f48
JustTheInstinct <jordangmain@gmail.com>
2022-02-17 20:14:23 -0800

    update auth methiod to use JSON file (user=admin, password=potato)

--- auth.py
+++ auth.py
@@ -1,11 +1,19 @@
 import json
 
-def credentials(user, password):
-    if user == 'admin' and password == 'potato':
+def credentials():
+    # Function reads and store JSON dict into variable
+    with open('credential.json', 'r') as file:
+        info = file.read()
+        info = json.loads(info)
+
+    # Basic authentication
+    if info['user'] == 'admin' and info['password'] == 'potato':
+        print("1")
         return 1
     else:
+        print("0")
         return 0
 
 
 if __name__ == "__main__":
-    pass
+    auth = credentials()
