e37d858732e94123246019f861319e9c5f6c356d
JustTheInstinct <jordangmain@gmail.com>
2022-02-20 12:07:59 -0800

    gitignore, Flaskapp, credential example, updated DockerFile

---
+++ app.py
@@ -0,0 +1,19 @@
+import time, auth
+from flask import Flask
+
+from auth import credentials
+
+app = Flask(__name__)
+
+@app.route('/')
+def test():
+    auth_check = credentials
+    if auth_check:
+        print("Valid")
+        return "Validated"
+    else:
+        print("Invalid")
+        return "Invalid credentials"
+
+if __name__ == "__main__":
+    test()
