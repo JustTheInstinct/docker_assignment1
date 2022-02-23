4a405710e60b839e8a63f87631ed5e4bba07afcf
jaspreet0708 <64561183+jaspreet0708@users.noreply.github.com>
2022-02-23 03:03:42 -0800

    added mongo db data to html page

--- show-data/server.js
+++ show-data/server.js
@@ -8,6 +8,18 @@ var MongoClient = require('mongodb').MongoClient;
 
 var url = "mongodb://mongodb:27017/";
 
+const content = document.getElementById("content")
+
+MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    var dbo = db.db("data");
+    dbo.collection("info").find({}).toArray(function(err, result) {
+        if (err) throw err;
+        console.log(result);
+        content.innerHTML(result.words)
+        db.close();
+    });
+    });
 
 
 function checkauth(username, password){
@@ -56,15 +68,10 @@ app.post('/login', async (req, res) =>{
     }
 })
 
-MongoClient.connect(url, function(err, db) {
-    if (err) throw err;
-    var dbo = db.db("data");
-    dbo.collection("info").find({}).toArray(function(err, result) {
-      if (err) throw err;
-      console.log(result);
-      db.close();
-    });
-  });
+
+
+
+
 
 app.listen(9000, () => {
     console.log("app running on port 9000");
