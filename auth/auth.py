2df928fa3c7151187b965fa47a391095e9608aab
jaspreet0708 <64561183+jaspreet0708@users.noreply.github.com>
2022-04-05 20:12:55 -0700

    i dead

--- auth/auth.py
+++ auth/auth.py
@@ -1,32 +1,32 @@
-import json
-
-from flask import Flask, request
-
-
+from flask import Flask
+from flask import request
 
 app = Flask(__name__)
 
-@app.route('/', method = ['POST'])
+@app.route('/')
 def credentials():
-    global result
-    # Function reads and store JSON dict into variable
-    info = request.get_json()
+    username = request.args.get('username')
+    password = request.args.get('password')
+    return(f'user name is {username} and password is {password}')
+    # global result
+    # # Function reads and store JSON dict into variable
+    # info = request.get_json()
 
-    # Basic authentication
-    if info['username'] == 'Jaspreet' and info['password'] == 'Password':
-        print("1")
-        result = 1
-    elif info['username'] == 'Jordan' and info['password'] == 'Password':
-        print("1")
-        result = 1
-    elif info['username'] == 'Arthur' and info['password'] == 'Password':
-        print("1")
-        result = 1
-    else:
-        print("0")
-        result = 0
-    result = 1
-    return json.dumps({"result":result})
+    # # Basic authentication
+    # if info['username'] == 'Jaspreet' and info['password'] == 'Password':
+    #     print("1")
+    #     result = 1
+    # elif info['username'] == 'Jordan' and info['password'] == 'Password':
+    #     print("1")
+    #     result = 1
+    # elif info['username'] == 'Arthur' and info['password'] == 'Password':
+    #     print("1")
+    #     result = 1
+    # else:
+    #     print("0")
+    #     result = 0
+    # result = 1
+    # return json.dumps({"result":result})
 
 if __name__ == "__main__":
     app.run(port=5000)
